3678611cead7df5c3c319c62be7e3ac7ad1a4c7d
Intergalactyc <78564099+Intergalactyc@users.noreply.github.com>
2024-04-15 12:14:31 -0500

    Add files via upload

---
+++ demonstration.py
@@ -0,0 +1,6 @@
+from keygen import keypair
+import encrypt
+import decrypt
+
+if __name__ == "__main__":
+    
---
+++ encrypt.py
@@ -0,0 +1,12 @@
+import sys
+# argparse?
+
+
+# Encrypt a message using a public key
+
+def encrypt(message: str, key: tuple[int,int]) -> str:
+    pass
+
+if __name__ == "__main__:
+    # if a file is passed in, encrypt it (line by line? overall? depend on length???)
+    # if a string is passed in, encrypt it
